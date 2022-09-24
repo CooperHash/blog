5f1eba5650420a3d6a25d617a698c24ef40d4196
Cooper <2597905281@qq.com>
2022-09-24 14:55:55 +0800

    hoc组件及redux

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,8 +1,7 @@
 export const BUTTON_CLICK = 'BUTTON_CLICK'
 
-export function click(store) {
-    return {
-      type: BUTTON_CLICK,
-      store
-    }
+export function click() {
+  return {
+    type: BUTTON_CLICK
   }
+}
